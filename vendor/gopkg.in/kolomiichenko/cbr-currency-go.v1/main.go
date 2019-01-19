58667f353b152495d147cf351cbfbd236e38eedd
Pavel Derendyaev <pderendyaev@alfabank.ru>
2019-01-19 19:59:42 +0300

    Update cbr library from my fork

--- vendor/gopkg.in/kolomiichenko/cbr-currency-go.v1/main.go
+++ vendor/gopkg.in/kolomiichenko/cbr-currency-go.v1/main.go
@@ -12,6 +12,13 @@ import (
 	"golang.org/x/net/html/charset"
 )
 
+// Original URL - http://www.cbr.ru/scripts/XML_daily.asp
+// Unofficial mirror - https://www.cbr-xml-daily.ru/daily.xml, but without past dates
+const URL = "http://www.cbr.ru/scripts/XML_daily.asp"
+
+// date_req = Date of query (dd/mm/yyyy)
+const DF = "02/01/2006"
+
 type xmlResult struct {
 	ValCurs xml.Name `xml:"ValCurs"`
 	Date    string   `xml:"Date,attr"`
@@ -52,17 +59,28 @@ func doEvery(d time.Duration, f func()) {
 	}
 }
 
-// GetCurrencyRates Get map of rates
+// GetCurrencyRates Return cached map of rates
 func GetCurrencyRates() map[string]currencyRate {
 	return currencyRates
 }
 
-// UpdateCurrencyRates To sync rates from CBR server
+// UpdateCurrencyRates Sync today currency rates with CBR
 func UpdateCurrencyRates() {
-	resp, err := http.Get("http://www.cbr.ru/scripts/XML_daily.asp")
+	mu.Lock()
+	currencyRates = FetchCurrencyRates(time.Time{})
+	defer mu.Unlock()
+}
+
+// FetchCurrencyRates Fetch map of rates for specified date
+func FetchCurrencyRates(d time.Time) map[string]currencyRate {
+	url := URL
+	if !d.IsZero() {
+		url = url + "?date_req=" + d.Format(DF)
+	}
+	resp, err := http.Get(url)
 	if err != nil {
 		log.Printf("Error of get currency: %v", err.Error())
-		return
+		return nil
 	}
 
 	var data xmlResult
@@ -72,17 +90,15 @@ func UpdateCurrencyRates() {
 	err = decoder.Decode(&data)
 	if err != nil {
 		log.Printf("error: %v", err)
-		return
+		return nil
 	}
 
-	mu.Lock()
-
-	currencyRates = make(map[string]currencyRate)
+	rates := make(map[string]currencyRate)
 
 	for _, el := range data.Valute {
 		value, _ := strconv.ParseFloat(strings.Replace(el.Value, ",", ".", -1), 64)
 
-		currencyRates[el.CharCode] = currencyRate{
+		rates[el.CharCode] = currencyRate{
 			ID:      el.ID,
 			NumCode: el.NumCode,
 			ISOCode: el.CharCode,
@@ -91,5 +107,5 @@ func UpdateCurrencyRates() {
 		}
 	}
 
-	defer mu.Unlock()
+	return rates
 }
